8676b0e6f47bda13336e4247e94b1e9874276e44
Tabor Alemu <56770999+talemu@users.noreply.github.com>
2022-04-20 23:25:29 -0500

    Delete Final Project/main/java/edu/iit/cs445/spring2022/restlamp directory

--- Final Project/main/java/edu/iit/cs445/spring2022/restlamp/LampDemo.java
+++
@@ -1,9 +0,0 @@
-package edu.iit.cs445.spring2022.restlamp;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("api")
-public class LampDemo extends Application {
-	// Intentionally left empty for this demo
-}
--- Final Project/main/java/edu/iit/cs445/spring2022/restlamp/NullLamp.java
+++
@@ -1,8 +0,0 @@
-package edu.iit.cs445.spring2022.restlamp;
-
-public class NullLamp extends Lamp {
-    @Override
-    public boolean isNil() {
-        return true;
-    }
-}
