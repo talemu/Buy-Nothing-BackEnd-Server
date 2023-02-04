bfcbb955f7d680f906b2e56e4cc5a9b43c6637dd
Tabor Alemu <56770999+talemu@users.noreply.github.com>
2023-02-03 18:06:04 -0600

    Delete HW1 directory

--- HW1/Flyer.java
+++
@@ -1,7 +0,0 @@
-package HW1.gradle.cs445Repo;
-
-
-public interface Flyer {
-	// acts as a template for creatures who can fly (Bat, Fly)
-	public void fly();
-}
--- HW1/README.md
+++
@@ -1,2 +0,0 @@
-# cs445Repo
-This is my official repository for CS445.
